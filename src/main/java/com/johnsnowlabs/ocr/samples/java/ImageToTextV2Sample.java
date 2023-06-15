7cb9d74581abe20c5045e158c71fea02b05a21a8
xyutech <klg-testing@yandex.ru>
2023-06-15 22:03:50 +0300

    Changed text detector to V1

--- src/main/java/com/johnsnowlabs/ocr/samples/java/ImageToTextV2Sample.java
+++ src/main/java/com/johnsnowlabs/ocr/samples/java/ImageToTextV2Sample.java
@@ -8,6 +8,7 @@ import org.apache.spark.sql.SparkSession;
 
 import com.johnsnowlabs.ocr.ImageType;
 import com.johnsnowlabs.ocr.transformers.BinaryToImage;
+import com.johnsnowlabs.ocr.transformers.ImageTextDetector;
 import com.johnsnowlabs.ocr.transformers.recognizers.ImageToTextV2;
 
 import sparkocr.transformers.detectors.ImageTextDetectorV2;
@@ -22,15 +23,25 @@ public class ImageToTextV2Sample {
 	        binaryToImage.setOutputCol("image");
 	        binaryToImage.setImageType(ImageType.TYPE_3BYTE_BGR());
 
-	        ImageTextDetectorV2 text_detector = 
-	        		(ImageTextDetectorV2) ImageTextDetectorV2
-	        		.pretrained("image_text_detector_v2", "en", "clinical/ocr");
-	        text_detector.setInputCol("image") ;
-	        text_detector.setOutputCol("text_regions") ;
-	        text_detector.setSizeThreshold(-1) ;
-	        text_detector.setLinkThreshold(0.3);
-	        text_detector.setWidth(500);
-	        text_detector.setWithRefiner(true);
+	        ImageTextDetector textDetector = 
+	        		(ImageTextDetector) ImageTextDetector
+	        		.pretrained("text_detection_v1", "en", "clinical/ocr");
+	        textDetector.setInputCol("image");
+	        textDetector.setOutputCol("text_regions");
+	        textDetector.setSizeThreshold(10);
+	        textDetector.setScoreThreshold(0.9);
+	        textDetector.setLinkThreshold(0.4);
+	        textDetector.setTextThreshold(0.2);
+
+//	        ImageTextDetectorV2 textDetector = 
+//	        		(ImageTextDetectorV2) ImageTextDetectorV2
+//	        		.pretrained("image_text_detector_v2", "en", "clinical/ocr");
+//	        textDetector.setInputCol("image") ;
+//	        textDetector.setOutputCol("text_regions") ;
+//	        textDetector.setSizeThreshold(-1) ;
+//	        textDetector.setLinkThreshold(0.3);
+//	        textDetector.setWidth(500);
+//	        textDetector.setWithRefiner(true);
 
 	        ImageToTextV2 imageToTexV2t = 
 	        		ImageToTextV2
@@ -43,7 +54,7 @@ public class ImageToTextV2Sample {
 	        Pipeline pipeline = new Pipeline();
 	        pipeline.setStages(new PipelineStage[] {
 	        		binaryToImage,
-	        		text_detector,
+	        		textDetector,
 	        		imageToTexV2t
 	        		});
 
